60d21b7c408d3cb545121c15380899e478a81f3c
Nader Elhaj <naderelhaj3@gmail.com>
2022-07-20 14:17:17 +0100

    Bulding Sidebar and Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,30 @@
-import type { NextPage } from 'next'
-
-
-const Home: NextPage = () => {
+import axios from 'axios'
+import { Video } from '../types'
+interface IProps {
+  videos : Video[]
+}
+const Home = ({videos}:IProps) => {
   return (
-    <div >
-      TIKTOK
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length ? videos.map(video => ( 
+        videos.map((video:Video) => (
+          <VideoCard post={video} key={video._id} />
+        ))
+      )):(
+        <NoResult text={'No Videos'} />
+      )}
+      
     </div>
   )
 }
 
+export const getServerSideProps = async () => {
+  const {data} = await axios.get(`http://localhost:3000/api/post`);
+  return{
+    props: {
+      videos:data
+    }
+  }
+
+}
 export default Home
