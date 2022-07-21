dc7731ac6b391b53972f72789a9d9fef5ce76131
Nader Elhaj <naderelhaj3@gmail.com>
2022-07-21 19:07:00 +0100

    Google Auth Implementation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { GoogleLogin, googleLogout } from '@react-oauth/google'
 import {AiOutlineLogout} from 'react-icons/ai'
 import {BiSearch} from 'react-icons/bi'
 import {IoMdAdd} from 'react-icons/io'
 import Logo from '../utils/tiktik-logo.png'
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
+
+const user = false ; 
 const Navbar = () => {
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
@@ -20,6 +22,14 @@ const Navbar = () => {
                 />
             </div>
         </Link>
+        <div>
+          Search
+        </div>
+        {user ? (
+          <div>aa</div>
+        ) : (
+          <GoogleLogin onSuccess={(response)=>{console.log(response)}} onError={()=>{console.log('error')}} />
+        )}
     </div>
   )
 }
