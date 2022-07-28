7e425b3d0ed192be057c93232f0b5e566f7ec10b
Nader Elhaj <naderelhaj3@gmail.com>
2022-07-28 16:48:02 +0100

    Implementing Search Functionality and video filtering by topic

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -9,9 +9,20 @@ import Logo from "../utils/tiktik-logo.png";
 import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { createOrGetUser } from "../utils";
 import useAuthStore from "../store/authStore";
+import { IUser } from "../types";
 
 const Navbar = () => {
-  const { userProfile, addUser,removeUser } = useAuthStore();
+  const { userProfile, addUser, removeUser } = useAuthStore();
+  const router = useRouter()
+
+  const [searchValue, setSearchValue] = useState("")
+
+  const handleSearch = (e:{preventDefault:()=> void} ) => {
+    e.preventDefault()
+    if(searchValue){
+      router.push(`/search/${searchValue}`)
+    }
+  };
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -24,7 +35,25 @@ const Navbar = () => {
           />
         </div>
       </Link>
-      <div>Search</div>
+
+      <div className="relative hidden md:block">
+        <form
+          onSubmit={handleSearch}
+          className="absolute md:static top-10 -left-20 bg-white"
+        >
+          <input
+            type="text"
+            value={searchValue}
+            onChange={(e) => {setSearchValue(e.target.value)}}
+            placeholder="Search Accounts and Videos"
+            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full md:first-letter:top-0   "
+          />
+          <button onClick={handleSearch} className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400 " >
+            <BiSearch />
+          </button>
+        </form>
+      </div>
+
       {userProfile ? (
         <div className="flex gap-5 md:gap-10">
           <Link href="/upload">
@@ -46,12 +75,15 @@ const Navbar = () => {
               </>
             </Link>
           )}
-          <button type="button" className="px-2" onClick={()=>{
-            googleLogout();
-            removeUser()
-
-          }}>
-            <AiOutlineLogout  color="red" fontSize={21} />
+          <button
+            type="button"
+            className="px-2"
+            onClick={() => {
+              googleLogout();
+              removeUser();
+            }}
+          >
+            <AiOutlineLogout color="red" fontSize={21} />
           </button>
         </div>
       ) : (
